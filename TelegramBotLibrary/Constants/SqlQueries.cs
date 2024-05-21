28c9cdb6a8c88614d56d8464eaaa7ee59746b944
CucerOriginal <hafizov.danilzte@gmail.com>
2024-05-22 00:25:04 +0300

    Изменил sql запросы для postgres.

--- TelegramBotLibrary/Constants/SqlQueries.cs
+++ TelegramBotLibrary/Constants/SqlQueries.cs
@@ -1,50 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TelegramBotLibrary.Constants
+﻿namespace TelegramBotLibrary.Constants
 {
 	internal class SqlQueries
 	{
-		public const string InsertBuyer = "INSERT " +
-			"INTO Buyer (Username, ChannelId) " +
-			"VALUES (@Username, (" +
-			"	SELECT Id" +
-			"	FROM Channel" +
-			$"	WHERE Name LIKE @ChannelName))";
+		public const string InsertBuyer = "INSERT INTO public.\"Buyer\" (\"Username\", \"ChannelId\") " +
+								  "VALUES (@Username, (" +
+								  "    SELECT c.\"Id\" " +
+								  "    FROM public.\"Channel\" c " +
+								  "    WHERE c.\"Name\" LIKE @ChannelName))";
 
-		public const string UpdateBuyer = "UPDATE Buyer " +
-			"SET ChannelId = (" +
-			"	SELECT Id" +
-			"	FROM Channel" +
-			"	WHERE Name LIKE @ChannelName) " +
-			"WHERE Username LIKE @Username";
+		public const string UpdateBuyer = "UPDATE public.\"Buyer\" b " +
+										  "SET \"ChannelId\" = (" +
+										  "    SELECT c.\"Id\" " +
+										  "    FROM public.\"Channel\" c " +
+										  "    WHERE c.\"Name\" LIKE @ChannelName) " +
+										  "WHERE b.\"Username\" LIKE @Username";
 
-		public const string SelectBuyer = "SELECT * " +
-			"FROM Buyer " +
-			$"WHERE Username LIKE @Username";
+		public const string SelectBuyer = "SELECT b.* " +
+										  "FROM public.\"Buyer\" b " +
+										  "WHERE b.\"Username\" LIKE @Username";
 
-		public const string SelectCostChannelByUsername = "SELECT Cost " +
-			"FROM Channel " +
-			"WHERE Id = (" +
-			"	SELECT ChannelId" +
-			"	FROM Buyer" +
-			"	WHERE Username LIKE @Username)";
+		public const string SelectCostChannelByUsername = "SELECT c.\"Cost\" " +
+														   "FROM public.\"Channel\" c " +
+														   "WHERE c.\"Id\" = (" +
+														   "    SELECT b.\"ChannelId\" " +
+														   "    FROM public.\"Buyer\" b " +
+														   "    WHERE b.\"Username\" LIKE @Username)";
 
-		public const string UpdateBuyerInvoicedIdByUsername = "UPDATE Buyer " +
-			"SET InvoiceId = @InvoiceId " +
-			"WHERE Username LIKE @Username";
+		public const string UpdateBuyerInvoicedIdByUsername = "UPDATE public.\"Buyer\" " +
+															   "SET \"InvoiceId\" = @InvoiceId " +
+															   "WHERE \"Username\" LIKE @Username";
 
-		public const string SelectInvoiceIdByUsername = "SELECT InvoiceId " +
-			"FROM Buyer " +
-			"WHERE Username LIKE @Username";
+		public const string SelectInvoiceIdByUsername = "SELECT \"InvoiceId\" " +
+														 "FROM public.\"Buyer\" " +
+														 "WHERE \"Username\" LIKE @Username";
 
-		public const string SelectChannelUrl = "SELECT Url " +
-			"FROM Channel " +
-			"WHERE Id = (SELECT ChannelId " +
-			"	FROM Buyer " +
-			"	WHERE Username LIKE @Username)";
+		public const string SelectChannelUrl = "SELECT c.\"Url\" " +
+											   "FROM public.\"Channel\" c " +
+											   "WHERE c.\"Id\" = (" +
+											   "    SELECT b.\"ChannelId\" " +
+											   "    FROM public.\"Buyer\" b " +
+											   "    WHERE b.\"Username\" LIKE @Username)";
+
 	}
 }
